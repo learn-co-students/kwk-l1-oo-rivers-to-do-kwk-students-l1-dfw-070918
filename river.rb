b701fea60a9ba42bdb06d883a5af761e1d18f7e5
jessicamh13 <jessica.mh13@yahoo.com>
2018-07-13 19:15:12 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,18 @@
 # river.rb
+class River 
+  attr_reader :name, :length, :countries, :discharge
+  attr_writer :name, :length, :countries, :discharge
+  
+  def initialize(name)
+    @name = name
+  end 
+
+  def flood
+    @discharge *= 1.30
+  end 
+  
+  def dry_up
+    @discharge *= 0.50
+  end
+  
+end 
